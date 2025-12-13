4ef5599947b4217e6eee5ee0e319ebd81687cd09
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-12-13 05:47:49 +0400

    Add configuration handling for Chromium with JSON support

--- main.py
+++ main.py
@@ -9,13 +9,62 @@
 # dependencies = ["freesimplegui>=5.2.0.post1"]
 # ///
 
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Self
+
 import FreeSimpleGUI as sg
 
 
+# Types
+class Type:
+    FLAG = "flag"
+    STRING = "string"
+
+
+@dataclass
+class Arg:
+    ui_label: str
+    ui_desc: str
+    enabled: bool
+    arg: str
+    type: Type
+    value: str | None = None
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Self:
+        """Create an Arg instance from a dictionary."""
+        return cls(**data)
+
+    def __str__(self) -> str:
+        if self.type == Type.STRING:
+            assert self.value
+            return f"{self.arg}=\"{self.value}\""
+
+        if self.type == Type.FLAG:
+            return self.arg
+
+
+class Config:
+    def __init__(self, filepath: Path):
+        json_data = json.loads(filepath.read_text())
+        self.browser_path: Path = Path(json_data.get("browser_path", ""))
+        self.args: list[Arg] = [Arg.from_dict(arg_data) for arg_data in json_data.get("args", [])]
+
+
+def read_all_configs() -> list[Config]:
+    config_files = Path(__file__).parent.glob("*.config.json")
+    return [Config(filepath) for filepath in config_files]
+
+
 def main():
+    # Read configuration
+    configs = read_all_configs()
+    print(f"Loaded {len(configs)} configuration(s).")
+    print(configs)
     # Create a simple window with a "Hello World" label
     layout = [[sg.Text("Hello World")]]
-
     window = sg.Window("Hello World App", layout)
 
     # Event loop
